93778fa499af7ae71535e4b4996a9fea383d8ba1
Adi Aggarwal <Adi.Aggarwal@godigit.com>
2024-08-04 18:58:01 +0530

    [ADI] Adding UC1

---
+++ src/Contact.java
@@ -0,0 +1,99 @@
+
+public class Contact{
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String city;
+    private int zip;
+    private long phone_no;
+    private String email;
+    private String state;
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", phone_no=" + phone_no +
+                ", email='" + email + '\'' +
+                '}';
+    }
+    public Contact(String firstname, String lastname, String address, String city, String state, int zip, long phone_no, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone_no = phone_no;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhone_no() {
+        return phone_no;
+    }
+
+    public void setPhone_no(long phone_no) {
+        this.phone_no = phone_no;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
